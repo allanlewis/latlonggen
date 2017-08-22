80f50290a8a677bc46e75d7d0097cbfe841bc4d2
Allan Lewis <allanlewis99@gmail.com>
2017-08-22 21:30:16 +0100

    Initial commit

---
+++ latlonggen.py
@@ -0,0 +1,44 @@
+import csv
+import googlemaps
+import logging
+from gooey import Gooey, GooeyParser
+from operator import itemgetter
+from unidecode import unidecode
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)8s: %(message)s')
+logger = logging.getLogger(__name__)
+
+
+@Gooey(program_name='Lat-Lng Generator')
+def main():
+    parser = GooeyParser()
+    parser.add_argument(
+        'input_file', widget='FileChooser', metavar='Input file',
+        help='A text file containing a list of place names', default='places.txt')
+    parser.add_argument(
+        'output_file', widget='FileSaver', metavar='Output file',
+        help='A CSV file to write the results to', default='places.csv')
+    parser.add_argument('api_key', metavar='Google Maps Geocoding API key')
+    args = parser.parse_args()
+
+    gmaps = googlemaps.Client(key=args.api_key)
+    with open(args.input_file) as input_file, open(args.output_file, 'w', newline='') as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow(('Place', 'Latitude', 'Longitude'))
+        for place in (unidecode(line.strip()).strip() for line in input_file if not line.startswith('#')):
+            try:
+                response = gmaps.geocode(place)
+                if not response:
+                    print(f'No result for {place!r}!')
+                    continue
+
+                lat, lng = itemgetter('lat', 'lng')(response[0]['geometry']['location'])
+                print(f'{place}: ({lat}, {lng})')
+                writer.writerow((place, lat, lng))
+            except Exception as exc:
+                print(f'Exception while geocoding {place!r}: {exc}')
+                continue
+
+
+if __name__ == '__main__':
+    main()
